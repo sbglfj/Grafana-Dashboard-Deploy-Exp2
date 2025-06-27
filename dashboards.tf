707e787e32fb01016aa1875b59bac57e53ccc9b7
sbglfj <jpx2b1@gmail.com>
2025-06-27 15:23:25 -0400

    Update dashboards.tf

--- dashboards.tf
+++ dashboards.tf
@@ -1,21 +1,19 @@
 resource "grafana_dashboard" "elasticsearch" {
-  provider = grafana.cloud
+      provider = grafana.cloud
 
-  config_json = file("${path.module}/266_rev4.json")
-  folder      = grafana_folder.ElasticSearch.id
-}
-resource "grafana_dashboard" "influxdb" {
-  provider = grafana.cloud
+      config_json = file("${path.module}/266_rev4.json")
+      folder      = grafana_folder.ElasticSearch.id
+    }
+    resource "grafana_dashboard" "influxdb" {
+      provider = grafana.cloud
 
-  for_each    = fileset("${path.module}/dashboards/influxdb", "707_rev8.json")
-  config_json = file("${path.module}/dashboards/influxdb/${each.key}")
-  folder      = grafana_folder.InfluxDB.id
-}
+      config_json = file("${path.module}/1150_rev2.json")
+      folder      = grafana_folder.InfluxDB.id
+    }
 
-resource "grafana_dashboard" "aws" {
-  provider = grafana.cloud
+    resource "grafana_dashboard" "aws" {
+      provider = grafana.cloud
 
-  for_each    = fileset("${path.module}/dashboards/aws", "1150_rev2.json")
-  config_json = file("${path.module}/dashboards/aws/${each.key}")
-  folder      = grafana_folder.AWS.id
-}
+      config_json = file("${path.module}/707_rev8.json")
+      folder      = grafana_folder.AWS.id
+    }
